c081e4544a07ad73f105d3461696e55f3e114d5d
Augusto Soares <soaresaugusto@edu.unifor.br>
2024-10-31 19:53:38 -0300

    organizing config

--- backend/app/config.py
+++ backend/app/config.py
@@ -5,13 +5,13 @@ class Config:
     SECRET_KEY = os.getenv('SECRET_KEY', 'o-paulo-ta-no-limite-das-faltas')
     
     #Exemplo de configuração do bd, será alterado para o mysql
-    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL', 'mysql://root:dbDev!6421@localhost/medcare')
+    SQLALCHEMY_DATABASE_URI        = os.getenv('DATABASE_URL', 'mysql://root:dbDev!6421@localhost/medcare')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
 def mydb = mysql.connector.connect(
-    host="localhost",
-    user="root",
-    passwd="dbDev!6421",
-    database="medcare",
-    auth_plugin='mysql_native_password'
+    host        ="localhost",
+    user        ="root",
+    passwd      ="dbDev!6421",
+    database    ="medcare",
+    auth_plugin ='mysql_native_password'
 )
